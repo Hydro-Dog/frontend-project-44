7e2fb0db842af05975bc8b67e552a144d2f50ebc
Vlad Babaev <vladislav.babaev@distillery.com>
2022-10-22 13:05:03 +0300

    task-4-eslint

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Hydro-Dog/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/Hydro-Dog/frontend-project-44/actions)
+
+<a href="https://codeclimate.com/github/Hydro-Dog/frontend-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/cb4f242eb24e0b03e9df/maintainability" /></a>
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,11 @@
 import readlineSync from 'readline-sync';
 
-export const startGame = () => {
-    console.log('Welcome to the Brain Games!')
+const startGame = () => {
+  console.log('Welcome to the Brain Games!');
 
-    const name = readlineSync.question('May I have your name? ')
-    
-    console.log(`Hello, ${name}!`)
-}
+  const name = readlineSync.question('May I have your name? ');
+
+  console.log(`Hello, ${name}!`);
+};
+
+export default startGame;
